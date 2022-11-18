3c270176e973ad003adca5921387b4d8878f70b3
Craig Donald <1craig.donald@gmail.com>
2022-11-18 14:04:48 +0000

    Final

--- spec/03_todo_spec.rb
+++ spec/03_todo_spec.rb
@@ -10,13 +10,13 @@ RSpec.describe "Todo" do
 
         it 'returns true if task is done' do
             newtask = Todo.new("Clean car")
-            newtask.mark_done!
-            expect(newtask.done?).to eq true
+            newtask.mark_complete
+            expect(newtask.complete?).to eq true
         end
 
         it "returns false if task isn't done" do
             newtask = Todo.new("Hoover the roof")
-            expect(newtask.done?).to eq false
+            expect(newtask.complete?).to eq false
         end
     end
 end
